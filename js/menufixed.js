fd92e6340b287ec638f0e922522bc31a3bd977e7
Enzo Catalán Vargas <enzocv96@gmail.com>
2016-01-02 15:20:24 -0500

    cambios en menu

--- js/menufixed.js
+++ js/menufixed.js
@@ -1,14 +1,15 @@
 $(document).ready(function(){
     var altura = $('.menu').offset().top;
+    var altura2 = $('.icon-tito').offset().top;
 
     $(window).on('scroll', function(){
         if ( $(window).scrollTop() > altura ){
             $('.menu').addClass('menu-fixed');
-            
+
         }
         else {
             $('.menu').removeClass('menu-fixed');
-            
+
         }
     });
 
